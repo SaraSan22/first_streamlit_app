4d29a024ba2eeb7640b4c88956e8c9cf98def29f
SaraSan22 <121622180+SaraSan22@users.noreply.github.com>
2022-12-29 15:39:29 +0100

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -4,3 +4,7 @@ streamlit.header('Breakfast Menu🍞🥗 ')
 streamlit.text('Omega 3 milk')
 streamlit.text('Scrumbled eggs🐔')
 streamlit.header('Build your own fuit smoothie🥑')
+
+importar pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
